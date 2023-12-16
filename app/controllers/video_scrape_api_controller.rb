f83bce5b483db2c524ed5b891ace4414627efe32
aivideosearch <aivideosearch@skiff.com>
2023-12-16 16:05:45 -0600

    view for video. Help pg search next

--- app/controllers/video_scrape_api_controller.rb
+++ app/controllers/video_scrape_api_controller.rb
@@ -1,2 +1,9 @@
 class VideoScrapeApiController < ApplicationController
+    def index
+        @video = Video.first
+        @meta = @video.video_metadata[0]
+        @transcript = @video.transcripts[0]
+        @channel = Channel.find(@meta.channel_id)
+        @platform = Platform.find(@channel.platform_id)
+    end
 end
