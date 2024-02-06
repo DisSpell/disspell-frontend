9a19b1fa6c81bd4771c9c4eee2ae72f1da2b94de
aivideosearch <aivideosearch@skiff.com>
2024-02-05 23:13:38 -0600

    fucking sprockets and importmaps pileline what pieces of shit. Useless undocumented nonsense that does not work as expected in any way whatsoever

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "perfect-scrollbar", to: "https://ga.jspm.io/npm:perfect-scrollbar@1.5.5/dist/perfect-scrollbar.esm.js"
+pin_all_from "app/javascript/custom", under: "custom"
