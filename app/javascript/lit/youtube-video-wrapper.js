d5e781680fd024660aff50a652bedb61accae08d
aivideosearch <aivideosearch@skiff.com>
2024-02-08 18:31:41 -0600

    youtube video wrapper initial setup functions work. Still no luck onStatePlayerChange beyond the initial loading. Unsure how events are setup or propagate through the web component and the YT.Player

--- app/javascript/lit/youtube-video-wrapper.js
+++ app/javascript/lit/youtube-video-wrapper.js
@@ -26,17 +26,52 @@ class YoutubeVideoWrapper extends LitElement {
   constructor() {
     super();
     this.videoId = '';
+    this.player;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    // Load the YouTube iframe API script dynamically
+    const tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    window.onYouTubeIframeAPIReady = this.onYouTubeIframeAPIReady.bind(this);
+  }
+
+  onYouTubeIframeAPIReady() {
+    this.player = new YT.Player('youtube-player', {
+        videoId: this.videoId,
+        events: {
+          'onReady': this.onPlayerReady(event),
+          'onStateChange': this.onPlayerStateChange(this)
+        }
+    });
+    console.log('new player ready?');
+  }
+
+  onPlayerReady(event) {
+    // document.getElementById('youtube-player').style.borderColor = '#FF6D00';
+    console.log('hi')
+  }
+
+  onPlayerStateChange(event) {
+    // changeBorderColor(event.data);
+    console.log('state change?');
+    console.log(event);
   }
 
   render() {
     return html`
       <iframe
+        id="youtube-player"
         width="560"
         height="315"
         src="https://www.youtube.com/embed/${this.videoId}"
         title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        frameborder="1"
+        origin="http://localhost:3000"
+        allow="clipboard-write; encrypted-media; picture-in-picture"
         allowfullscreen
       ></iframe>
     `;
