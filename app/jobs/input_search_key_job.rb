93e290aa2a19fcbd7d0ce45698e7d21400889f26
aivideosearch <aivideosearch@skiff.com>
2024-03-12 09:45:56 -0600

    attempt move to hetzner

--- app/jobs/input_search_key_job.rb
+++ app/jobs/input_search_key_job.rb
@@ -8,9 +8,10 @@ class InputSearchKeyJob < ApplicationJob
     # Do something later
     puts args[0]
 
-    url = "http://localhost:3001/videos"
+    url = "http://45.56.66.213/videos"
     # url = "http://45.79.10.188/video_scrape_api"
     uri = URI(url)
     request = Net::HTTP.post(uri, args[0].to_json, headers = {'Accept' => 'application/json', 'Content-Type' => "application/json"})
+    puts request
   end
 end
