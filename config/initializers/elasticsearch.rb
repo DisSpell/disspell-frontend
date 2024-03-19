e92e30ff4cffeb1e983f6b55a425ac32bc47895f
aivideosearch <aivideosearch@skiff.com>
2024-03-19 15:40:14 -0600

    Holy Batman's Balls did I get Elastic Search Setup finally?
    Have not setup https yet

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,1 +1,12 @@
-Elasticsearch::Model.client = Elasticsearch::Client.new(url: ENV['ELASTICSEARCH_URL'] || 'http://localhost:9200', password: ENV['ELASTIC_PASSWORD'])
+if Rails.env.production?
+  Elasticsearch::Model.client = Elasticsearch::Client.new(
+    url: ENV['ELASTICSEARCH_URL'],
+    user: ENV['ELASTICSEARCH_USER'],
+    password: ENV['ELASTICSEARCH_PASSWORD']
+  )
+else
+  # Default to localhost:9200 for development and test environments
+  Elasticsearch::Model.client = Elasticsearch::Client.new(
+    url: 'http://localhost:9200'
+  )
+end
