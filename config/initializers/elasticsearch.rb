14f6d788c5bea5624949e0e221056be2d09e6357
aivideosearch <aivideosearch@skiff.com>
2024-03-19 20:03:09 -0600

    holy fuck let this be the light

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,10 +1,7 @@
-Rails.logger.info "Elasticsearch URL: #{ENV.fetch('ELASTIC_URL', 'not set')}"
-Rails.logger.info "Elasticsearch User: #{ENV.fetch('ELASTIC_USER', 'not set')}"
-
 if Rails.env.production?
   Elasticsearch::Model.client = Elasticsearch::Client.new(
     url: ENV['ELASTIC_URL'],
-    http_auth: [ENV['ELASTIC_USER'], ENV['ELASTIC_PASSWORD']]
+    http: {[ENV['ELASTIC_USER'], ENV['ELASTIC_PASSWORD']]}
   )
 else
   # Default to localhost:9200 for development and test environments
