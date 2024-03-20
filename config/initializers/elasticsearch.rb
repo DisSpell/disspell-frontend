fea0e88df533db31b88ddef6eebb39fa192ab922
aivideosearch <aivideosearch@skiff.com>
2024-03-19 20:07:15 -0600

    fucking syntax errors

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,7 +1,7 @@
 if Rails.env.production?
   Elasticsearch::Model.client = Elasticsearch::Client.new(
     url: ENV['ELASTIC_URL'],
-    http: {[ENV['ELASTIC_USER'], ENV['ELASTIC_PASSWORD']]}
+    http: {[ENV['ELASTIC_USER'], ENV['ELASTIC_PASSWORD']}
   )
 else
   # Default to localhost:9200 for development and test environments
