44ca927928a09cc8be064d5a9b40d7af298ed259
aivideosearch <aivideosearch@skiff.com>
2024-03-21 10:13:12 -0600

    sitemap and robot.txt

---
+++ config/sitemap.rb
@@ -0,0 +1,34 @@
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "http://www.disspell.com"
+
+SitemapGenerator::Sitemap.create do
+  # Put links creation logic here.
+  #
+  # The root path '/' and sitemap index file are added automatically for you.
+  # Links are added to the Sitemap in the order they are specified.
+  #
+  # Usage: add(path, options={})
+  #        (default options are used if you don't specify)
+  #
+  # Defaults: :priority => 0.5, :changefreq => 'weekly',
+  #           :lastmod => Time.now, :host => default_host
+  #
+  # Examples:
+  #
+  # Add '/articles'
+  #
+  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
+  #
+  # Add all articles:
+  #
+  #   Article.find_each do |article|
+  #     add article_path(article), :lastmod => article.updated_at
+  #   end
+
+
+  Video.find_each do |video|
+    add video_path(video), lastmod: video.updated_at
+  end
+
+
+end
