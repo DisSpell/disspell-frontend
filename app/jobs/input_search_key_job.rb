141ca1b870092d9c8e87e30793728ac9e9ee35af
aivideosearch <aivideosearch@skiff.com>
2024-03-26 15:54:37 -0600

    change call to backend ip

--- app/jobs/input_search_key_job.rb
+++ app/jobs/input_search_key_job.rb
@@ -8,7 +8,7 @@ class InputSearchKeyJob < ApplicationJob
     # Do something later
     puts args[0]
 
-    url = "http://45.56.66.213/videos"
+    url = "http://5.78.72.39/videos"
     # url = "http://45.79.10.188/video_scrape_api"
     uri = URI(url)
     request = Net::HTTP.post(uri, args[0].to_json, headers = {'Accept' => 'application/json', 'Content-Type' => "application/json"})
