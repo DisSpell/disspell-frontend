c0e43d1ab050a6c123f47500e1b4fce9993added
aivideosearch <aivideosearch@skiff.com>
2024-03-26 10:51:05 -0600

    slight cleanup start maybe use unicode characters for logo?

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,12 +2,6 @@ class SearchController < ApplicationController
   def index
     if params[:query].present?
       @response = Search.search(params[:query]).response.hits.hits
-        
-      #postgres shit
-      @video = Video.search_video(params[:query])
-      @transcript = Transcript.search_transcript(params[:query])  
-      #result is the video ids of matched searches
-      @result = (@video.map(&:id) + @transcript.map(&:video_id))
     end
   end
 
