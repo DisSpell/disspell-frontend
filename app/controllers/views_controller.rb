3823131e1d9ec200717002a5634673bbe039ad5b
aivideosearch <aivideosearch@skiff.com>
2024-04-23 12:31:28 -0600

    video views in the last day

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -5,22 +5,28 @@ class ViewsController < ApplicationController
 
 
         @chart = Ahoy::Event
-        .where('time > ?', 3.months.ago)
-        .where_properties(controller: "videos")
-        .group_by_day(:time)
-        .count
+            .where('time > ?', 3.months.ago)
+            .where_properties(controller: "videos")
+            .group_by_day(:time)
+            .count
 
         @search = Ahoy::Event
-        .where('time > ?', 3.months.ago)
-        .where_properties(controller: "search")
-        .group_by_day(:time)
-        .count
+            .where('time > ?', 3.months.ago)
+            .where_properties(controller: "search")
+            .group_by_day(:time)
+            .count
 
         @top_videos = Ahoy::Event
-        .where('time > ?', 3.months.ago)
-        .where_properties(controller: "videos", action: "show")
-        .map {|e| e.properties["id"]}.tally
-        .sort_by {|k,v| -v}
+            .where('time > ?', 3.months.ago)
+            .where_properties(controller: "videos", action: "show")
+            .map {|e| e.properties["id"]}.tally
+            .sort_by {|k,v| -v}
+
+        @newest_videos = Ahoy::Event
+            .where('time > ?', 1.day.ago)
+            .where_properties(controller: "videos", action: "show")
+            .map {|e| e.properties["id"]}.tally
+            .sort_by {|k,v| -v}
     end 
 
 end
