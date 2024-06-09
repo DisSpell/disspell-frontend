97ef00d6820947f82e070284e78e8316d88dbf22
aivideosearch <aivideosearch@skiff.com>
2024-06-09 11:02:13 -0600

    kaminari pagination of pages

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
   def index
     if params[:query].present?
       @search = Search.search(params[:query]).records
-      @response = @search.page(params[:page]).per(1)
+      @response = @search.page(params[:page]).per(10)
     end
   end
 
